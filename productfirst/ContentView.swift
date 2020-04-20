62e088ea11e4db02dbd28df7aebd5e6d9458ea3e
Martin <martin@macbook-pro-de-martin.home>
2020-04-20 15:04:49 +0200

    First Commit

---
+++ productfirst/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  productfirst
+//
+//  Created by Martin on 20/04/2020.
+//  Copyright © 2020 Martin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Christophe !")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
